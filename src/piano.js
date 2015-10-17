2a4c030e345bc0fb2b8f2dc6a2e8c987a68f504e
Esteban Manchado Velázquez <emanchado@demiurgo.org>
2015-10-17 21:21:15 +0200

    Simple key/scale selectors and crude note highlighting

--- src/piano.js
+++ src/piano.js
@@ -1,16 +1,19 @@
-var React = require('react'),
-    ReactDOM = require('react-dom'),
-    R = require("ramda");
+const React = require('react'),
+      ReactDOM = require('react-dom'),
+      R = require("ramda"),
+      teoria = require("teoria");
 
 const NUMBER_OCTAVES = 2;
 
-var noteNames = {
+const noteNames = {
     "c#": "C# D♭",
     "d#": "D# E♭",
     "f#": "F# G♭",
     "g#": "G# A♭",
     "a#": "A# B♭"
 };
+
+const initialScale = 'major', initialKey = 'c';
 
 function noteLabel(note) {
     if (noteNames.hasOwnProperty(note)) {
@@ -25,18 +28,20 @@ function noteAudioUrl(octaveNumber, note) {
 }
 
 /*jshint ignore:start */
-var PianoKey = React.createClass({
+const PianoKey = React.createClass({
     render: function() {
-        var className = this.props.note.length > 1 ? 'ebony' : '',
-            octaveNumber = this.props.octave,
-            label = noteLabel(this.props.note),
-            audioUrl = noteAudioUrl(octaveNumber, this.props.note);
+        const keyTypeClassName = this.props.note.length > 1 ? 'ebony' : '',
+              highlightClassName = this.props.highlightScale ? 'in-scale' : '',
+              className = keyTypeClassName + ' ' + highlightClassName,
+              octaveNumber = this.props.octave,
+              label = noteLabel(this.props.note),
+              audioUrl = noteAudioUrl(octaveNumber, this.props.note);
 
         return (
-                <li className={className} onClick={this.handleClick}>
+            <li className={className} onClick={this.handleClick}>
                 {label}
                 <audio ref="audioEl" preload="auto" src={audioUrl} />
-                </li>
+            </li>
         );
     },
 
@@ -51,15 +56,26 @@ var PianoKey = React.createClass({
     }
 });
 
-var Piano = React.createClass({
+function noteInScale(noteName, scale) {
+    const note = teoria.note(noteName);
+
+    return scale.notes().some(function(scaleNote) {
+        return scaleNote.chroma() === note.chroma();
+    });
+}
+
+const Piano = React.createClass({
     render: function() {
-        var octaveKeys = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#",
-                          "a", "a#", "b"];
-        var keys = R.flatten(R.range(0, NUMBER_OCTAVES).map(function(octaveNumber) {
+        const octaveKeys = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#",
+                            "a", "a#", "b"];
+        const currentScale = teoria.note(this.props.scalekey + '4').scale(this.props.scale);
+        const keys = R.flatten(R.range(0, NUMBER_OCTAVES).map(function(octaveNumber) {
             return octaveKeys.map(function(note) {
-                let reactKey = (octaveNumber + 1) + "-" + note;
+                let reactKey = (octaveNumber + 1) + "-" + note,
+                    inScale = noteInScale(note, currentScale);
                 return (
-                        <PianoKey octave={octaveNumber+1} key={reactKey} note={note} />
+                        <PianoKey octave={octaveNumber+1}
+                    key={reactKey} note={note} highlightScale={inScale} />
                 );
             });
         }));
@@ -72,9 +88,57 @@ var Piano = React.createClass({
 });
 /*jshint ignore:end */
 
+const MusicExplorerApp = React.createClass({
+    getInitialState: function() {
+        return {scale: initialScale, key: initialKey};
+    },
+
+    render: function() {
+        return (
+                <div>
+                <Piano scale={this.state.scale} scalekey={this.state.key} />
+
+                <div class="tools">
+                <select value={this.state.key} onChange={this.onChangeKey}>
+                <option value="c">C</option>
+                <option value="c#">C# / D♭</option>
+                <option value="d">D</option>
+                <option value="d#">D# / E♭</option>
+                <option value="e">E</option>
+                <option value="f">F</option>
+                <option value="f#">F# / G♭</option>
+                <option value="g">G</option>
+                <option value="g#">G# / A♭</option>
+                <option value="a">A</option>
+                <option value="a#">A# / B♭</option>
+                <option value="b">B</option>
+                </select>
+
+                <select value={this.state.scale} onChange={this.onChangeScale}>
+                <option value="major">Major</option>
+                <option value="minor">Minor</option>
+                <option value="blues">Blues</option>
+                <option value="phrygian">Phrygian</option>
+                </select>
+                </div>
+                </div>
+        );
+    },
+
+    onChangeScale: function(e) {
+        const newScale = e.target.options[e.target.selectedIndex].value;
+        this.setState({scale: newScale});
+    },
+
+    onChangeKey: function(e) {
+        const newKey = e.target.options[e.target.selectedIndex].value;
+        this.setState({key: newKey});
+    }
+});
+
 ReactDOM.render(
     /*jshint ignore:start */
-        <Piano />,
+        <MusicExplorerApp />,
     /*jshint ignore:end */
     document.getElementById('contents')
 );
